32941a858b229b82c12144e43c66ad6b384d0c01
Tao-Tao-real <2510737554@qq.com>
2020-12-24 00:52:27 +0800

    ba hu

--- 20模拟/八皇后问题/main.cpp
+++ 20模拟/八皇后问题/main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-19 15:01:28
- * @LastEditTime: 2020-12-19 15:10:16
+ * @LastEditTime: 2020-12-24 00:50:20
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /projects/AlgorithmDesign/20模拟/八皇后问题/main.cpp
@@ -14,21 +14,45 @@
 
 using namespace std;
 
-bool isvalid(vector<vector<int>>& pos){
+int board[8];
+int rescount = 0;
+void print_board(){
+    cout << "No. " << rescount << endl;
     for(int i = 0; i < 8; ++i){
-        for(int j = i; j < 8; ++j){
-            if(pos[i][1] == pos[j][1] || abs(pos[i][0]-pos[j][0]) == abs(pos[i][1]-pos[j][1])){
-                return false;
+        for(int j = 0; j < 8; ++j){
+            if(board[j] == i){
+                cout << 1 << " ";
+            }else{
+                cout << 0 << " ";
             }
         }
+        cout << endl;
+    }
+}
+
+bool is_ok(int i, int j){
+    for(int index = 0; index < i; ++index){
+        if(board[index] == j) return false;
+        if(abs(i-index) == abs(j - board[index])) return false;
     }
     return true;
 }
 
-int main(){
-    vector<vector<int>> pos(8, vector<int>(2,0));
-    for(int i = 0; i < 8; ++i){
-        pos[i][0] = i;
+void place(int i){
+    if(i == 8){
+        rescount++;
+        print_board();
+    }else{
+        for(int j = 0; j < 8; ++j){
+            board[i] = j;
+            if(is_ok(i, j)){
+                place(i+1);
+            }
+        }
     }
-    traceback(pos, )
+}
+
+int main(){
+    place(0);
+    return 0;
 }
